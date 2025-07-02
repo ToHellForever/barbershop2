a09f003b3e5c515453749123668009f43a1760a7
ToHellForever <dim.anosoff2018@yandex.ru>
2025-07-02 17:38:16 +0300

    hw_40: Модель услуги

--- core/models.py
+++ core/models.py
@@ -28,9 +28,22 @@ class Master(models.Model):
     photo = models.ImageField(upload_to="masters/", blank=True, verbose_name="Фотография")
     phone = models.CharField(max_length=20, verbose_name="Телефон")
     address = models.CharField(max_length=255, verbose_name="Адрес")
-    experience = models.PositiveIntegerField(verbose_name="Опыт работы")
+    experience = models.PositiveIntegerField(verbose_name="Стаж работы", help_text="Опыт работы в годах")
     services = models.ManyToManyField('Service', related_name="masters", verbose_name="Услуги")
     is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    duration = models.PositiveIntegerField(verbose_name="Длительность", help_text="Время выполнения в минутах")
+    is_popular = models.BooleanField(default=False, verbose_name="Популярная услуга")
+    image = models.ImageField(upload_to="services/", blank=True, verbose_name="Изображение")
 
     def __str__(self):
         return self.name
