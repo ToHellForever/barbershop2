875f633fafce54ba1948dd7672984ec3b885e67b
ToHellForever <dim.anosoff2018@yandex.ru>
2025-07-02 17:36:37 +0300

    hw_40: Модель мастера

--- core/models.py
+++ core/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="not_approved", verbose_name="Статус")
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     date_updated = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
-    master = models.ForeignKey(Master, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Мастер")
+    master = models.ForeignKey('Master', on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Мастер")
     services = models.ManyToManyField(Service, related_name="orders", verbose_name="Услуги")
     appointment_date = models.DateTimeField(verbose_name="Дата и время записи")
 
@@ -23,3 +23,14 @@ class Order(models.Model):
         return f"{self.client_name} - {self.appointment_date}"
     
     
+class Master(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Имя")
+    photo = models.ImageField(upload_to="masters/", blank=True, verbose_name="Фотография")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    address = models.CharField(max_length=255, verbose_name="Адрес")
+    experience = models.PositiveIntegerField(verbose_name="Опыт работы")
+    services = models.ManyToManyField('Service', related_name="masters", verbose_name="Услуги")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    def __str__(self):
+        return self.name
